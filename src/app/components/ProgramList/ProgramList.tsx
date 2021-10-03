7435b22dd75e146297bb8c0fa063dc4fd670ec3a
Pavel Khovalkin <khovalkin@radyushin.com>
2021-10-03 12:50:31 +0400

    add try/catch to fetch

--- src/app/components/ProgramList/ProgramList.tsx
+++ src/app/components/ProgramList/ProgramList.tsx
@@ -3,18 +3,27 @@ import Program from './Program/Program'
 import { TServerData, TProgram } from '@/app/types'
 
 export default async function ProgramList() {
-	const data = await fetch('https://api.moscow.mba/products?_limit=100')
-	const dataJSON: TServerData[] = await data.json()
+	let dataFormatted: TProgram[] = []
 
-	const dataFormatted: TProgram[] = formatProgram(dataJSON)
+	try {
+		const response = await fetch('https://api.moscow.mba/products?_limit=100')
+
+		if (!response.ok) {
+			throw new Error(`Fetch failed: ${response.status} ${response.statusText}`)
+		}
+
+		const dataJSON: TServerData[] = await response.json()
+		dataFormatted = formatProgram(dataJSON)
+	} catch (error) {
+		console.error('Ошибка при загрузке программ:', error)
+		return <div>Ошибка при загрузке данных</div>
+	}
 
 	return (
 		<>
-			{dataFormatted.map((item: TProgram) => {
-				{
-					return <Program key={item.id} {...item} />
-				}
-			})}
+			{dataFormatted.map((item: TProgram) => (
+				<Program key={item.id} {...item} />
+			))}
 		</>
 	)
 }
